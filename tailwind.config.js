e2fe8cef7d1a4de85203b66bf71c8f6d67b5a758
Fermin <fermin.unicen@gmail.com>
2022-05-18 12:31:24 -0300

    background-image

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    //extender el tema que actualmente esta por defecto en tailwind, como por ejemplo, poner un background image
+    extend: {
+      //propiedades q extienden el tema van aca
+      backgroundImage: (theme) => ({
+        //si quiero utilizar esta extension, debo anteponer bg en la clase, que es la propiedad original
+        //de esta fomra quedaria en el html class="bg-body-pattern"
+        "body-pattern": "url('/assets/img/pattern.png')",
+      }),
+    },
   },
   variants: {
     extend: {},
